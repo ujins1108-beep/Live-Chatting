531793a6e98033bf282e2697f1da808dc54be237
SeoWJ <ujins1108@gmail.com>
2026-09-20 15:35:07 +0900

    헤더에 네비게이션 기능 추가

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,18 @@
 import './header.css'
+import { useNavigate } from 'react-router-dom'
 
 export default function Header() {
+    const navigate = useNavigate()
+
     return (
         <> 
             <div className='header-box'>
                 <div className='header-left' style={{marginRight: 'auto'}}>
-                    <h1 style={{margin: 0}}>Live Chatting</h1>
+                    <h1 style={{margin: 0}}>KkeuJeock</h1>
                 </div>
                 <div className='header-right' style={{marginLeft: 'auto'}}>
-                    <h1 style={{margin: 0}}>홈</h1>
-                    <h1 style={{margin: 0}}>커뮤니티</h1>
+                    <h1 style={{margin: 0}} onClick={() => {navigate('/')}}>홈</h1>
+                    <h1 style={{margin: 0}} onClick={() => {navigate('/community')}}>커뮤니티</h1>
                 </div>
             </div>
         </>
